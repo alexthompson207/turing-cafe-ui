2ee42f146ecec1d9aa3c68172d29b8cce70eaebc
Alex Thompson <ahthomps1@gmail.com>
2021-04-07 09:20:48 -0600

    Add basic data to Card component to display name, date, time, and number of guests

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,8 +4,16 @@ import './Card.css';
 const Card = () => {
 
   return (
-    
+    <section className='card'>
+      <h3>name</h3>
+      <p>date</p>
+      <p>time</p>
+      <p>Number of guests: number</p>
+      <button>Cancel</button>
+    </section>
   )
 }
 
 export default Card;
+
+// [{ id: 18907224, name: 'Christie', date: '8/8', time: '7:00', number: 3 }]

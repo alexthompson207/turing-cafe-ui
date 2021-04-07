93fbd55e515bd069ca494037c22e78ae4f02df3e
Alex Thompson <ahthomps1@gmail.com>
2021-04-07 11:27:37 -0600

    Add post request to seperate api file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,6 +5,14 @@ const getAllReservations = () => {
     .then(response => response.json())
 }
 
+const postResy = (newResy) => {
+  return fetch(`${baseURL}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newResy)
+  })
+    .then(response => response.json())
+}
 
 
-export { getAllReservations };
+export { getAllReservations, postResy };

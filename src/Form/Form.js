7fef49e51240fe04f8d8a45042e352dc8234ae0d
Alex Thompson <ahthomps1@gmail.com>
2021-04-07 09:50:38 -0600

    Create a basic class component for a reservation Form, with form and inputs and a button to create a new reservation

---
+++ src/Form/Form.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './Form.css'
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: ''
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name'
+          value={this.state.name}
+        />
+        <input
+          type='text'
+          name='date'
+          placeholder='Date (mm/dd)'
+          value={this.state.date}
+        />
+        <input
+          type='text'
+          name='time'
+          placeholder='Time'
+          value={this.state.time}
+        />
+        <input
+          type='text'
+          name='number'
+          placeholder='Number of guests'
+          value={this.state.number}
+        />
+        <button className='resy-btn'>Make Reservation</button>
+      </form>
+    )
+  }
+}

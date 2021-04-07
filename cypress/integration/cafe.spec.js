a374d6aee59c9a97a19f9d62768edf0f1c8ec21d
Alex Thompson <ahthomps1@gmail.com>
2021-04-07 11:50:45 -0600

    Add sad path for when user doesn't fill out inputs and clicks make reservation btn

--- cypress/integration/cafe.spec.js
+++ cypress/integration/cafe.spec.js
@@ -107,4 +107,29 @@ describe('Turing Cafe', () => {
     cy.get('#99 .cancel-btn').click();
     cy.get('.booking-view').children().should('have.length', '3');
   });
+});
+
+describe('Turing Cafe Erros', () => {
+
+  beforeEach(() => {
+
+    cy.intercept({
+      method: 'GET',
+      url: 'http://localhost:3001/api/v1/reservations'
+    },
+      {
+        statusCode: 201,
+        body: [
+          { 'id': 1, 'name': 'Coleslaw', 'date': '8/8', 'time': '7:00', 'number': 3 },
+          { 'id': 2, 'name': 'Fridge', 'date': '10/08', 'time': '10:30', 'number': 2 },
+          { 'id': 3, 'name': 'Smith', 'date': '03/08', 'time': '12:30', 'number': 1 }
+        ]
+      });
+    cy.visit('http://localhost:3000');
+  });
+
+  it.only('should display an error if user clicks make reservation button without filling out inputs ', () => {
+    cy.get('.resy-btn').click();
+    cy.get('h2').contains('Please enter the correct inputs');
+  });
 })
